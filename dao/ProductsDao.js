235e6103bbbbac332ea763709baac2ee1bf82e24
Shrey Agrawal <agrawalshrey10@gmail.com>
2022-07-18 00:56:13 +0530

    used ssl and rejectUnautrised: false pool changed to client
    self signed certificate error resolved

--- dao/ProductsDao.js
+++ dao/ProductsDao.js
@@ -1,11 +1,11 @@
 // const Products = require("../models/ProductModel");
-const pool = require("../db/DbConnection");
+const client = require("../db/DbConnection");
 const ProductsMapper = require('../mapper/ProductsMapper');
 
 function ProductsDao() {
     const find = function () {
         return new Promise((resolve, reject) => {
-            pool.query(
+            client.query(
                 'SELECT * FROM Products_table;',
                 (error, results) => {
                     if (error) {
--- db/DbConnection.js
+++ db/DbConnection.js
@@ -1,13 +1,13 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-    user: 'ewvtubinelzuyc',
-    host: 'ec2-52-205-61-230.compute-1.amazonaws.com',
-    database: 'ddgpscudnqidhv',
-    password: 'c74862f54d2fcbddb4ae5088e0df5cf88e26bc2d61c1e9b1ef50323aeb843610',
-    port: 5432,
-})
+const { Client } = require('pg');
+
+const client = new Client({
+connectionString: 'postgres://ewvtubinelzuyc:c74862f54d2fcbddb4ae5088e0df5cf88e26bc2d61c1e9b1ef50323aeb843610@ec2-52-205-61-230.compute-1.amazonaws.com:5432/ddgpscudnqidhv',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
 
 
-
-
-module.exports = pool;
+module.exports = client;
